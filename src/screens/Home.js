3cd3a56230089622e5321decd07ff1eb5b128033
Bikramjeet Singh <officialbikramjeet@gmail.com>
2023-09-03 19:42:37 +0530

    IC

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <div><Navbar /></div>
+        <div>yha body aayegi</div>
+        <div><Footer /></div>
+    </div>
+  )
+}
